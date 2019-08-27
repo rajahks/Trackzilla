febc0c12d3d98ca0d02a8f6729de2146c2c0e8af
akshata494 <akshata.krishnan@gmail.com>
2019-08-27 09:37:07 -0700

    Moving base html and css to a Generic folder

--- asseto/settings.py
+++ asseto/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'asseto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_URL = os.path.join(BASE_DIR, "static")
+
 
 # LOGGING configuration
 LOGGING = {
